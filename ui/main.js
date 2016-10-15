95033df985dfe5d91d82e0bfe1345894d77d49cd
Pinaki Nath Chowdhury <pinakinc@outlook.com>
2016-10-15 12:45:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,15 +10,21 @@ button.onclick = function(){
   request.onreadystatechange = function(){
       if (request.readyState === XMLHttpRequest.DONE){
           if (request.status === 200){
-              var counter = request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
+      var names=request.responseText;
+      names=JSON.parse(names);
+var list = '';
+
+for (var i=0;i< names.length;i++){
+    list +='<li>' +names[i] + '</li>';
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+    }
       }
   };
   
   //render the value to the website
-  request.open('GET','http://pinakinathc.imad.hasura-app.io/counter', true);
+  request.open('GET','http://pinakinathc.imad.hasura-app.io/submit-name?name=', true);
   request.send(null);
 };
 
@@ -27,18 +33,3 @@ button.onclick = function(){
 var nameInput = document.getElementById('name');
 var name = nameInput.value ;
 var submit = document.getElementById('submit_btn');
-
-submit.onclick = function(){
-//Make a request to the server and sent the name
-
-//Capture a list of names and render it as a list
-var names=['name1','name2','name3'];
-var list = '';
-
-for (var i=0;i< names.length;i++){
-    list +='<li>' +names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
-    
-};

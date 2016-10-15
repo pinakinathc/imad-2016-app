3567b423ab59b887b5494e771e2a173d1133cfb4
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2016-10-15 01:17:26 +0000

    server changed

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+  res.send('Article 1 is requested and will be served');
+});
+
+
+app.get('/article-two',function(req,res){
+  res.send('Article 2 is requested and will be served');
+});
+
+
+app.get('/article-three',function(req,res){
+  res.send('Article 3 is requested and will be served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

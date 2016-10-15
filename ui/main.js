a54e7bf38e91eccaf96890203bf279ec4cb0f55b
Pinaki Nath Chowdhury <pinakinc@outlook.com>
2016-10-15 10:53:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
 //counter code
 
 var button = document.getElementById('counter');
-var counter =0;
+
 
 button.onclick = function(){
   //make a request to the counter endpoint
+  var request = new XMLHttpRequest();
   
   //capture response and store it in a variable
+  request.onreadystatechange = function(){
+      if (request.readyState === XMLHttpRequest.DONE){
+          if (request.status === 200){
+              var counter = request.requestText;
+          }
+      }
+  };
   
   //render the value to the website
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML = counter.toString();
+  request.open('GET','http://pinakinathc.imad.hasura-app.io/', true);
+  request.send(null);
 };

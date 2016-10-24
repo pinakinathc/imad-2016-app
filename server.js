1ba70425485bd9ee9d19dbdb1ceae36a467e112d
Pinaki Nath Chowdhury <pinakinc@outlook.com>
2016-10-24 13:42:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req,res) {
-  res.sent("Article One being sent");
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-
 app.get('/article-two', function (res,req) {
     res.sent("Article two requested and will be served");
 });

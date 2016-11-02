3ea28d63aca3eeb1ce57420eb98599ef5872df7b
Pinaki Nath Chowdhury <pinakinc@outlook.com>
2016-11-02 14:23:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,7 @@
-//counter code
+console.log('File getting changed');
 
-var button = document.getElementById('submit');
+var img = document.getElementById('madi');
 
-button.onclick = function(){
-  //make a request to the counter endpoint
-  var request = new XMLHttpRequest();
-  
-  //capture response and store it in a variable
-  request.onreadystatechange = function(){
-      if (request.readyState === XMLHttpRequest.DONE){
-          if (request.status === 200){
-      var names=request.responseText;
-      names=JSON.parse(names);
-var list = '';
-
-for (var i=0;i< names.length;i++){
-    list +='<li>' +names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
-    }
-      }
-  };
-  
-  //render the value to the website
-  request.open('GET','http://pinakinathc.imad.hasura-app.io/submit-name?name=', true);
-  request.send(null);
+img.onclick = function(){
+    img.style.margineLeft = '100px';
 };
-
-//Submit name
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value ;
-var submit = document.getElementById('submit_btn');

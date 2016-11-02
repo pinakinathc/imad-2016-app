64f5c6b94826e2685fedf9eb430290151d3304b4
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2016-11-02 18:24:17 +0530

    personal details added

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,13 @@ function moveRight() {
 img.onclick = function(){
     var interval = setInterval(moveRight, 50);
 };
+
+var counter = 0;
+
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+	counter = counter + 1;
+	var select = document.getElementById('count');
+	select.innerHTML = counter.toString();
+};

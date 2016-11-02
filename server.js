bf1547340e5890bdbabf531a63f00cfd4327263c
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2016-11-03 03:22:35 +0530

    linked to database

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+	user: 'pinakinathc',
+	database: 'pinakinathc',
+	host: 'http://db.imad.hasura-app.io/',
+	port: '5432',
+	password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req,res){
+	//make a select request
+
+	//return a response with the results
+	pool.query('SELECT * FROM test', function(err, result) {
+		if (err) {
+			res.status(500).send(err.toString());
+		}
+		else{
+			res.send(JSON.stringify(result));
+		}
+	});
 });
 
 app.get('/article-one', function (req, res) {
@@ -59,6 +84,8 @@ app.get('/submit-name', function(req,res){
 	
 	res.send(JSON.stringify(names));
 });
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
